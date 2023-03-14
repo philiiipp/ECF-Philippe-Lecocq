40c2f40be35ce65cda30edfd9241531a5a908d96
Phil <lecocqp05@gmail.com>
2023-03-14 14:01:39 +0400

    formulaires sont en php

--- insertion.php
+++ insertion.php
@@ -1,7 +1,11 @@
 <?php
 
 try {
-    $objetPdo = new PDO('mariadb:host=localhost;dbname=quai_antique_1','root','16205');
+    $objetPdo = new PDO('mariadb:host=localhost;dbname=quai_antique','root','');
+
+    $objetPdo->setAttribute(PDO:ATTR_ERRMODE, PDO:ERRMODE_EXCPTION);
+    echo "Connection success";
+
 } catch (PDOException $e) {
-    var_dump('Oh non');
+    echo "Error in connection";
 }
---
+++ qa-sign-in-form.php
@@ -0,0 +1,44 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Great+Vibes&family=Montserrat:wght@500&family=Playfair+Display&family=Raleway&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">   
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+        <meta charset="utf-8">
+        <title>S'inscrire | Le Quai Antique </title>
+        <link rel="stylesheet" href="qa-style.css">
+    </head>
+
+    <body class="form-background">
+        <div class="form-container">
+            <div class="form-title">
+                <img class="form-logo"src="./images-quai-antique/logo-qa-noir.png">
+                <p>Inscrivez-vous pour continuer</p>
+            </div>      
+            <form method="get" action="">
+                <div class="form-layout">
+                    <input class="form-grey-bar" type="email" id="email" name="email" placeholder="Saisissez votre adresse e-mail" required>
+                    <input class="form-grey-bar" type="password" id="password" name="password" placeholder="Saisissez votre mot de passe" required>
+                    <div class="form-row">
+                        <label for="nombre-de-convives">Nombre de convive par défaut :</label></br>
+                        <input type="number" id="nombre-de-convives" name="nombre-de-convives" min="1" max="8">
+                    </div>
+                    <div class="form-row">
+                        <label for="allergies">Allergies :</label></br>
+                        <input class="form-grey-bar"type="text" id="allergies" name="allergies" placeholder="Spécifiez vos allergies">
+                    </div>
+                </div>
+                <div class="form-submit-box">
+                    <input class="form-submit" type="submit" value="Continuer">
+                </div>            
+            </form>
+        </div>
+        <script type="text/javascript" src="qa-script.js"></script>
+    </body>
+</html>

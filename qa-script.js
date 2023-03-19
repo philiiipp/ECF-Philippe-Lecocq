1d0d5b89be1e322c308eb947a41efb3e9ecdfd65
Phil <lecocqp05@gmail.com>
2023-03-19 14:04:00 +0400

    media queries

--- qa-script.js
+++ qa-script.js
@@ -9,3 +9,15 @@ function closePopup(){
     popup.classList.remove("open-popup");
     document.body.classList.remove("disable-scrolling");
 }
+
+let headerPopup = document.getElementById("header-popup");
+
+function openHeaderPopup(){
+    headerPopup.classList.add("header-popup-open");
+    document.body.classList.add("disable-scrolling");
+}
+
+function closeHeaderPopup(){
+    headerPopup.classList.remove("header-popup-open");
+    document.body.classList.remove("disable-scrolling");
+}

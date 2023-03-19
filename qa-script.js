7378fa36c1594e20556f4d598f6b4f75b98d3ef5
Phil <lecocqp05@gmail.com>
2023-03-19 12:01:38 +0400

    scrolling disabled quand le popup est ouvert

--- qa-script.js
+++ qa-script.js
@@ -2,8 +2,10 @@ let popup = document.getElementById("popup");
 
 function openPopup(){
     popup.classList.add("open-popup");
+    document.body.classList.add("disable-scrolling");
 }
 
 function closePopup(){
     popup.classList.remove("open-popup");
+    document.body.classList.remove("disable-scrolling");
 }
